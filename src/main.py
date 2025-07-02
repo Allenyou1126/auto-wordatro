90d86142b011cdd146ddf871a681f06c3a6b0145
Allenyou <i@allenyou.wang>
2025-07-02 21:24:38 +0800

    fix: mime type error

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import flask_app
-
+import mimetypes
 
 if __name__ == "__main__":
+    # Fix
+    mimetypes.add_type("application/javascript", ".js", strict=True)
+    mimetypes.add_type("application/json", ".json", strict=True)
     flask_app.flask_app.run(host="0.0.0.0", port=5000)
